21f606ee5a64f025a669bf4f62929df5a0136863
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2024-04-03 21:42:47 +0300

    Set up Authentication Context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Authentication from './pages/Authentication';
-import { Routes } from 'react-router-dom';
+import {AuthContext, authReducer} from './context/auth_context/AuthProvider'
+import { useContext, useEffect } from 'react';
 
 function App() {
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  console.log(auth);
+
+  useEffect(() => {
+    // If the user is not authenticated, navigate to the Authentication page
+    if (!auth || !auth.isAuthenticated) {
+      navigate('/Authentication');
+    }
+  }, [auth, navigate]);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/"/>
-        <Route path="/Authentication" element={<Authentication/>}/>
+        <Route path="/" />
+        <Route path="/Authentication" element={<Authentication />} />
       </Routes>
     </div>
   );
